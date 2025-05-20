e6594b544f2bc30205556b1bcd34f07ac39e4aa3
Mislav Češnik <mislav.cesnik88@gmail.com>
2025-05-20 11:22:35 +0200

    Edit header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,31 +14,28 @@ const Header: React.FC<HeaderProps> = ({isLoggedIn, handleLogout}: HeaderProps) 
     <header className='bg-white shadow-md p-4 flex justify-between items-center md:px-8'>
       <h1 className='font-bold text-2xl text-gray cursor-pointer'>Lost & Found</h1>
       
-      <button className='md:hidden' onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? <X size={28} /> : <Menu size={28} />}
-      </button>
-      
-      <nav className={`absolute md:static top-16 left-0 w-full md:w-auto bg-white md:flex md:items-center md:space-x-10 p-6 md:p-0 shadow-md md:shadow-none transition-all duration-300 ease-in-out ${
-    isOpen ? 'block' : 'hidden'
-  }`}>
-        <ul className='flex flex-col md:flex-row md:gap-10 text-gray'>
-          <li className='header-item'>Home</li>
-          <li className='header-item'>My Profile</li>
-          <li className='header-item'>About</li>
-        </ul>
-      </nav>
-
-      {isLoggedIn ? (
-        <div className="hidden md:flex profile-icon flex-row items-center">
-          <button className='h-[32px] w-[32px] cursor-pointer' onClick={handleLogout}>
-            <img src="logout.png" alt="Profile"/>
+      {isLoggedIn && (
+        <>
+          <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <X size={28} /> : <Menu size={28} />}
           </button>
-        </div>
-      ) : (
-        <div className='hidden md:flex gap-2.5'>
-          <button className='header-btn-signin'>Sign In</button>
-          <button className='header-btn-signup'>Sign Up</button>
-        </div>
+
+          <nav className={`absolute md:static top-16 left-0 w-full md:w-auto bg-white md:flex md:items-center md:space-x-10 p-6 md:p-0 shadow-md md:shadow-none transition-all duration-300 ease-in-out ${
+            isOpen ? 'block' : 'hidden'
+          }`}>
+            <ul className="flex flex-col md:flex-row md:gap-10 text-gray">
+              <li className="header-item">Home</li>
+              <li className="header-item">My Profile</li>
+              <li className="header-item">About</li>
+            </ul>
+          </nav>
+
+          <div className="hidden md:flex profile-icon flex-row items-center">
+            <button className="h-[32px] w-[32px] cursor-pointer" onClick={handleLogout}>
+              <img src="logout.png" alt="Profile" />
+            </button>
+          </div>
+        </>
       )}
     </header>
   );
